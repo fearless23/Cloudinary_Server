acaa12db1524dcbfb5992bd2fb7a5ae859420686
Jaspreet Singh <jaspreetsingh2379@gmail.com>
2020-04-30 20:14:51 +0530

    #4 Commit: Folder Name Option
    - Pass Folder name to queryParams
    - Files are stored with original name

--- src/app.js
+++ src/app.js
@@ -14,19 +14,39 @@ app.get('/', (req, res) => {
 });
 
 const getFilePathsFromRequest = (requestFiles) => {
-  const files = Object.values(requestFiles)[0];
-  if (Array.isArray(files)) return files.map((file) => file.tempFilePath);
-  return [files.tempFilePath];
+  let files = Object.values(requestFiles)[0];
+  if (!Array.isArray(files)) files = [files];
+
+  return files.map((file) => {
+    const tmpName = file.name.split('.');
+    tmpName.splice(tmpName.length - 1, 1);
+    return {
+      path: file.tempFilePath,
+      name: tmpName.join(''),
+    };
+  });
 };
 
 app.post('/upload', async (req, res) => {
   const filePaths = getFilePathsFromRequest(req.files);
+  const folderName = req.query.folder;
   try {
-    const pArr$ = filePaths.map((filePath) => uploadToCloudinary(filePath));
-    const data = await Promise.all(pArr$);
-    res.json({ msg: 'Image Uploaded', error: false, data });
+    const pArr$ = filePaths.map((filePath) =>
+      uploadToCloudinary(filePath, folderName)
+    );
+    const result = await Promise.all(pArr$);
+    const data = result.map(({ secure_url, format, resource_type }) => ({
+      secure_url,
+      format,
+      resource_type,
+    }));
+    res.status(200).json({
+      msg: 'Image Uploaded',
+      error: false,
+      data,
+    });
   } catch (error) {
-    res.json({
+    res.status(400).json({
       msg: error.message || error || 'Image Upload Error',
       error: true,
     });
--- src/cloudinary.util.js
+++ src/cloudinary.util.js
@@ -1,22 +1,26 @@
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.config({ 
-  cloud_name: 'jassi', 
-  api_key: '848377833547884', 
-  api_secret: 'iGRqMMV7p_NBrwPNjgnbuDGbj5M' 
+cloudinary.config({
+  cloud_name: 'jassi',
+  api_key: process.env.CLOUDINARY_API_KEY || '848377833547884',
+  api_secret: process.env.CLOUDINARY_API_KEY || 'iGRqMMV7p_NBrwPNjgnbuDGbj5M',
 });
 
-const uploadToCloudinary = (file) => {
-  const p = new Promise((resolve, reject) => {
-    cloudinary.uploader.upload(file, function (err, result) {
-      if (err) {
-        console.log(err);
-        reject(err);
-      }
-      resolve(result);
-    });
-  });
-  return p;
+const options = (name, folderName = 'heroku_server') => ({
+  public_id: `${folderName}/${name}`,
+  overwrite: true,
+  // notification_url: 'https://mysite.example.com/notify_endpoint',
+});
+
+const uploadToCloudinary = async ({ path, name }, folderName) => {
+  const opts = options(name, folderName);
+  try {
+    return await cloudinary.uploader.upload(path, opts);
+  } catch (error) {
+    console.error('UPLOAD ERROR:', error);
+    // ERROR FOR USER
+    throw new Error(`${name} upload error:`);
+  }
 };
 
 module.exports = { uploadToCloudinary };
